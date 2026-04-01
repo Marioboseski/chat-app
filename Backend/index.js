e14ed17262cbfdfa343e40ac234111d12b14e536
Mario Boseski <crboseskimario@gmail.com>
2026-04-01 19:14:42 +0200

    Feat: implement real-time chat with socket.io

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,37 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...")
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id)
+
+  socket.on("sendMessage", (message) => {
+    console.log("Message received", message )
+
+    io.emit("receiveMessage", message);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id)
+  })
 })
 
 const PORT = 5000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 })
--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import { socket } from "./socket";
+
 const App = () => {
+
+  const [ message, setMessage ] = useState("");
+  const [ messages, setMessages ] = useState<string[]>([]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server:", socket.id)
+
+      socket.on("receiveMessage", (data) => {
+        setMessages((prev) => [...prev, data]);
+      })
+    })
+    return () => {
+      socket.off("connect")
+      socket.off("receiveMessage")
+    }
+  },[]);
+
+  const sendMessage = () => {
+    if(!message.trim()) return
+
+    socket.emit("sendMessage", message);
+    setMessage("");
+  }
+
   return (
     <div>
-      <p>Hello World</p>
+      <p>Chat App</p>
+
+      <div>
+        {messages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+
+      <input type="text"
+      value={message}
+      onChange={(e) => setMessage(e.target.value)}
+      placeholder="Type message..." />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
