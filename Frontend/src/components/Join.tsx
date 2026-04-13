cc61133d6d2a4f6d94c51613f0690704bc526ec8
Mario Boseski <crboseskimario@gmail.com>
2026-04-13 17:24:09 +0200

    Feat: allow joining chat with Enter key

--- Frontend/src/components/Join.tsx
+++ Frontend/src/components/Join.tsx
@@ -6,24 +6,32 @@ type Props = {
   joinChat: () => void,
 };
 
-const Join = ({username, setUsername, room, setRoom, joinChat }: Props) => {
+const Join = ({ username, setUsername, room, setRoom, joinChat }: Props) => {
 
   return (
-    <div className="flex flex-col justify-evenly items-center min-h-dvh">
-      <h2 className="text-2xl">Enter Username and join room chat!</h2>
-      <input type="text"
-        value={username}
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-        className="border-2 border-black p-1 rounded-md" />
+    <div className="flex justify-center items-center p-2 min-h-dvh w-full">
+      <div className="flex flex-col justify-evenly items-center min-h-[500px] w-full max-w-md border-2 border-black">
+        <h2 className="text-2xl">Enter Username and join room chat!</h2>
+        <input type="text"
+          value={username}
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          className="border-2 border-black p-1 rounded-md" />
 
-      <input type="text"
-        value={room}
-        onChange={(e) => setRoom(e.target.value)}
-        placeholder="Enter room"
-        className="border-2 border-black p-1 rounded-md" />
+        <input type="text"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          placeholder="Enter room"
+          className="border-2 border-black p-1 rounded-md"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              joinChat();
+            }
+          }}
+        />
 
-      <button onClick={joinChat} className="border-2 border-black p-1 rounded-md">Join</button>
+        <button onClick={joinChat} className="border-2 border-black p-1 rounded-md">Join</button>
+      </div>
     </div>
   );
 }
