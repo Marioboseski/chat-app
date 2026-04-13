c82cc3fe867902b7d13cf612840a1c26e53a1191
Mario Boseski <crboseskimario@gmail.com>
2026-04-13 17:44:32 +0200

    style: improve joining form styles

--- Frontend/src/components/Join.tsx
+++ Frontend/src/components/Join.tsx
@@ -11,20 +11,20 @@ const Join = ({ username, setUsername, room, setRoom, joinChat }: Props) => {
   const isDisabled = !username.trim() || !room.trim();
 
   return (
-    <div className="flex justify-center items-center p-2 min-h-dvh w-full">
-      <div className="flex flex-col justify-evenly items-center min-h-[500px] w-full max-w-md border-2 border-black">
+    <div className="flex justify-center items-center p-3 min-h-dvh w-full">
+      <form className="flex flex-col justify-evenly items-center text-center min-h-[500px] w-full max-w-md border-2 border-black rounded-lg">
         <h2 className="text-2xl">Enter Username and join room chat!</h2>
         <input type="text"
           value={username}
           placeholder="Username"
           onChange={(e) => setUsername(e.target.value)}
-          className="border-2 border-black p-1 rounded-md" />
+          className="border-2 border-black p-1 rounded-md w-full max-w-xs" />
 
         <input type="text"
           value={room}
           onChange={(e) => setRoom(e.target.value)}
           placeholder="Enter room"
-          className="border-2 border-black p-1 rounded-md"
+          className="border-2 border-black p-1 rounded-md w-full max-w-xs"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               joinChat();
@@ -32,9 +32,9 @@ const Join = ({ username, setUsername, room, setRoom, joinChat }: Props) => {
           }}
         />
 
-        <button onClick={joinChat} className={`border-2 border-black p-1 rounded-md
+        <button onClick={joinChat} className={`p-1 rounded-md w-full max-w-32 bg-gray-300 text-lg hover:bg-gray-400
         ${isDisabled ? "opacity-65 cursor-not-allowed" : ""}`}>Join</button>
-      </div>
+      </form>
     </div>
   );
 }
