a81ecc93e5728eb3ba5a171b66a8f2b9ea3d8bfe
Mario Boseski <crboseskimario@gmail.com>
2026-04-16 18:57:09 +0200

    Feat: add active user status

--- Frontend/src/components/SideBar.tsx
+++ Frontend/src/components/SideBar.tsx
@@ -1,4 +1,5 @@
 import { useChatStore } from "../store/chatStore";
+import { Circle } from "lucide-react";
 
 const SideBar = () => {
 
@@ -8,7 +9,10 @@ const SideBar = () => {
     <div className="w-1/4 border-r-2 border-gray-500 p-4">
       <h3 className="font-bold">Users in room:</h3>
       {users.map((user) => (
-        <p key={user.id}>{user.username}</p>
+        <div key={user.id} className="flex items-center gap-3">
+          <Circle size={10} className="bg-green-500 " />
+        <p>{user.username}</p>
+        </div>
       ))}
     </div>
   );
